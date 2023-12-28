e249369b909a0fad6b0c2ed9753d22f8b5eb11c8
choace0427 <jacobkevin0427@gmail.com>
2023-12-28 04:21:02 -0500

    fix api error

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,67 +1,67 @@
-'use client'
-import { useState } from 'react'
-import { Card, CardHeader, CardBody, CardFooter, Typography, Input, Checkbox, Button } from '@material-tailwind/react'
-import { ToastContainer, toast } from 'react-toastify'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
+'use client';
+import { useState } from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Typography, Input, Checkbox, Button } from '@material-tailwind/react';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter()
-  const [forget, setForget] = useState(false)
-  const [reset, setReset] = useState(false)
-  const [resetcode, setResetCode] = useState('')
-  const [resetpassword, setResetPassword] = useState('')
-  const [email, setEmail] = useState('')
+  const router = useRouter();
+  const [forget, setForget] = useState(false);
+  const [reset, setReset] = useState(false);
+  const [resetcode, setResetCode] = useState('');
+  const [resetpassword, setResetPassword] = useState('');
+  const [email, setEmail] = useState('');
 
-  const [signEmail, setSignEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [signEmail, setSignEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSignIn = async () => {
     if (signEmail && password) {
       const signinData = {
         email: signEmail,
         password: password,
-      }
+      };
 
       await axios
-        .post('http://localhost:4000/development/api/auth/signin', signinData)
+        .post(`${process.env.NEXT_PUBLIC_API}/auth/signin`, signinData)
         .then((res) => {
           if (res.data.statusCode === 500) {
-            toast.error(res.data.message)
+            toast.error(res.data.message);
           } else {
             if (res.data.AuthenticationResult) {
-              localStorage.setItem('user', JSON.stringify(res.data.AuthenticationResult))
-              toast.success('Sign In successfully')
-              router.push('/dashboard')
+              localStorage.setItem('user', JSON.stringify(res.data.AuthenticationResult));
+              toast.success('Sign In successfully');
+              router.push('/dashboard');
             }
           }
         })
         .catch((err) => {
-          console.log('err', err)
-        })
+          console.log('err', err);
+        });
     }
-  }
+  };
 
   const handleForgetPassword = async () => {
     if (email) {
       const forgetData = {
         email: email,
-      }
+      };
       await axios
-        .post('http://localhost:4000/development/api/auth/forgetpassword', forgetData)
+        .post(`${process.env.NEXT_PUBLIC_API}/auth/forgetpassword`, forgetData)
         .then((res) => {
           if (res.data.statusCode === 500) {
-            toast.error(res.data.message)
+            toast.error(res.data.message);
           } else {
-            toast.success('Register Successfull!')
-            setReset(true)
+            toast.success('Register Successfull!');
+            setReset(true);
           }
         })
         .catch((err) => {
-          console.log('err', err)
-        })
+          console.log('err', err);
+        });
     }
-  }
+  };
 
   const handleVerify = async () => {
     if (resetcode) {
@@ -69,22 +69,22 @@ export default function Home() {
         email: email,
         code: resetcode,
         password: resetpassword,
-      }
+      };
       await axios
-        .post('http://localhost:4000/development/api/auth/resetpassword', verificationData)
+        .post(`${process.env.NEXT_PUBLIC_API}/auth/resetpassword`, verificationData)
         .then((res) => {
           if (res.data.statusCode === 500) {
-            toast.error(res.data.message)
+            toast.error(res.data.message);
           } else {
-            toast.success(res.data.message)
-            setForget(true)
+            toast.success(res.data.message);
+            setForget(true);
           }
         })
         .catch((err) => {
-          console.log('err', err)
-        })
+          console.log('err', err);
+        });
     }
-  }
+  };
 
   return (
     <>
@@ -98,13 +98,7 @@ export default function Home() {
           </CardHeader>
           <CardBody className='flex flex-col gap-4'>
             <Input label='Email' size='lg' value={signEmail} onChange={(e) => setSignEmail(e.target.value)} />
-            <Input
-              label='Password'
-              type='password'
-              size='lg'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Input label='Password' type='password' size='lg' value={password} onChange={(e) => setPassword(e.target.value)} />
             <div className='w-full flex justify-between items-center'>
               <div className='-ml-2.5'>
                 <Checkbox label='Remember Me' />
@@ -154,21 +148,8 @@ export default function Home() {
                 </Typography>
               </CardHeader>
               <CardBody className='flex flex-col gap-4'>
-                <Input
-                  label='Verification Code'
-                  type='number'
-                  size='lg'
-                  onChange={(e) => setResetCode(e.target.value)}
-                  value={resetcode}
-                  inputMode='numeric'
-                />
-                <Input
-                  label='Reset Password'
-                  type='password'
-                  size='lg'
-                  onChange={(e) => setResetPassword(e.target.value)}
-                  value={resetpassword}
-                />
+                <Input label='Verification Code' type='number' size='lg' onChange={(e) => setResetCode(e.target.value)} value={resetcode} inputMode='numeric' />
+                <Input label='Reset Password' type='password' size='lg' onChange={(e) => setResetPassword(e.target.value)} value={resetpassword} />
               </CardBody>
               <CardFooter className='pt-0'>
                 <Button variant='gradient' fullWidth onClick={() => handleVerify()}>
@@ -180,5 +161,5 @@ export default function Home() {
         </>
       )}
     </>
-  )
+  );
 }
